43e7351b659a3bb514cca9cf8cd1b74f903983bf
randompersonlol12 <125095955+randompersonlol12@users.noreply.github.com>
2025-05-19 23:10:47 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // MAD  LIBS GAME
         Scanner scanner = new Scanner(System.in);
 
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
+        double radius;
+        double circumference;
+        double area;
+        double volume;
 
-        System.out.print("Enter an adjective (description): ");
-        adjective1 = scanner.nextLine();
-        System.out.print("Enter a noun (animal or person): ");
-        noun1 = scanner.nextLine();
-        System.out.print("Enter an adjective (description): ");
-        adjective2 = scanner.nextLine();
-        System.out.print("Enter a verb end with -ing (action): ");
-        verb1 = scanner.nextLine();
-        System.out.print("Enter an adjective (description): ");
-        adjective3 = scanner.nextLine();
+        System.out.print("Enter the radius: ");
+        radius = scanner.nextDouble();
 
-        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
-        System.out.println("In an exhibit, I saw a " + noun1 + ".");
-        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
-        System.out.println("I was " + adjective3 +"!");
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0/ 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.printf("The circumference is: %.1fcm\n" , circumference);
+        System.out.printf("The area is: %.1fcm^2\n" , area);
+        System.out.printf("The volume is: %.1fcm^3\n" , volume);
 
         scanner.close();
     }
